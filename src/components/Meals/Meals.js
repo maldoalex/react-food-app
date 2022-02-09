a961da2d63e6d0973f534d36b1af30b41be54d86
maldoalex <maldoalex@gmail.com>
2022-02-09 15:19:48 -0600

    Create AddNewMeal component

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,11 +1,13 @@
 import {Fragment} from 'react';
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
+import AddNewMeal from './AddNewMeal/AddNewMeal';
 
 const Meals = props => {
   return (
     <Fragment>
       <MealsSummary />
+      {/* <AddNewMeal /> */}
       <AvailableMeals />
     </Fragment>
   )
